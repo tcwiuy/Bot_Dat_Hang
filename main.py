1b0bb0b59d25cb15ec25a9a15cbcf84f8274d214
tcwiuy <tangcamminh2000@gmail.com>
2026-03-16 08:36:59 +0700

    Fix: Open CORS for Vercel deployment

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_origins=["http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
